d12dc86003169b891f3d543ab2a4b2b8035d3bcc
yunyunenara <dbswlsla07@naver.com>
2023-11-12 00:37:11 +0900

    Refactor: community repository refactoring

---
+++ star_hub/lib/community/model/entity/scope_post_entity.dart
@@ -0,0 +1,23 @@
+class ScopePostEntity {
+  String title;
+  String content;
+  String nickName;
+  String writeDate;
+  String level;
+  int likes;
+  int clips;
+  int comments;
+
+  ScopePostEntity({
+    required this.title,
+    required this.content,
+    required this.nickName,
+    required this.writeDate,
+    required this.level,
+    required this.likes,
+    required this.clips,
+    required this.comments,
+  });
+}
+
+
