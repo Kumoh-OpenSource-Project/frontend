4ddccf61f850c61f504b50917860db9ff0edadf9
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2023-11-26 23:37:40 +0900

    Revert "마이 페이지 작성"

--- star_hub/ios/Runner/GeneratedPluginRegistrant.m
+++ star_hub/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,37 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_secure_storage/FlutterSecureStoragePlugin.h>)
-#import <flutter_secure_storage/FlutterSecureStoragePlugin.h>
-#else
-@import flutter_secure_storage;
-#endif
-
-#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
-#import <image_picker_ios/FLTImagePickerPlugin.h>
-#else
-@import image_picker_ios;
-#endif
-
-#if __has_include(<kakao_flutter_sdk_common/KakaoFlutterSdkPlugin.h>)
-#import <kakao_flutter_sdk_common/KakaoFlutterSdkPlugin.h>
-#else
-@import kakao_flutter_sdk_common;
-#endif
-
-#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
-#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
-#else
-@import shared_preferences_foundation;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterSecureStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSecureStoragePlugin"]];
-  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
-  [KakaoFlutterSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"KakaoFlutterSdkPlugin"]];
-  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
 }
 
 @end
